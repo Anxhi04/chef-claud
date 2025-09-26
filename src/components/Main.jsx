f1ff055c62b69050824d4ab3107a2610def76d58
Anxhelika <krecianxhelika04@gmail.com>
2025-09-26 15:53:33 +0200

    lidhur me AI permes app paraqitja e recetes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,49 +1,38 @@
+import {getRecipeFromMistral} from "./ai";
 import React from "react";
+import IngredientList from "./IngredientList";
+import ClaudeRecipe from "./ClaudeRecipe";
+
 
 export default function Main(){
-    let [ingredients, setIngredient] =React.useState([])
+    let [ingredients, setIngredient] =React.useState(["patato", "oregano", "oil", "chicken"])
     const[recipeShown, setrecipeShown]=React.useState(false)
-    const ingredientList =ingredients.map(ingredient=>(
-        <li key={ingredient} >{ingredient}</li>
-    ))
-     
+    const[recipe, setrecipe]=React.useState("")
+
     function addIngredient(formData){
         const ingredient = formData.get("ingredient")
         setIngredient((previngredients)=>[...previngredients, ingredient])
         console.log(ingredient)
     }
-    function togglerecipeshown(){
-        setrecipeShown((oldrecipeval)=>!oldrecipeval)
+  
+    async function getrecipe(){
+            console.log("button clicked")
+            const reciperMarkdown = await getRecipeFromMistral(ingredients);
+            console.log(reciperMarkdown)
+            setrecipe(reciperMarkdown)
+            setrecipeShown(prevshown=>!prevshown)
+        
     }
     return(
         <main>
             
-            <form action={addIngredient} className="add-ingredient-form" >
+            <form action={addIngredient}  className="add-ingredient-form" >
                 <input type="text" placeholder="e.g. oregano" aria-label="Add ingredient" name="ingredient"/>
                 <button type="submit" >+ Add Ingredient</button>
             </form>
-        { ingredientList.length>0 ? 
-        <section>
-            <h2>Ingredients on hand:</h2>
-            <ul className="ingredient-list" aria-live="polite">{ingredientList}</ul>
-            {ingredientList.length>3 ? 
-            <div className="get-recipe-container">
-                <div>
-                    <h3>Ready for a recipe</h3>
-                    <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <div>
-                        <button onClick={togglerecipeshown}>Get a recipe </button>
-                    </div>
-            </div>: null
-                }
+        { ingredients.length>0 && <IngredientList ingredients={ingredients}  getrecipe={getrecipe}/>}
 
-        </section> : null}
-
-       {recipeShown && 
-        <div>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, sed blanditiis? In saepe ipsa consectetur quos voluptas eos iure sapiente error, eum dolores blanditiis, beatae nam, libero fuga dolor! Saepe dolor doloribus doloremque rerum eaque placeat ab qui esse nobis suscipit obcaecati ipsa error unde blanditiis animi reprehenderit, alias accusantium nulla impedit debitis veritatis dicta. Tempora cupiditate quos consectetur qui unde repudiandae, aperiam reprehenderit. Cum itaque sit obcaecati dolore non architecto amet quia veritatis debitis maxime atque, ad earum ex at sint aperiam. Dolore, voluptas. Veniam excepturi eum voluptatem temporibus hic unde harum nisi sed, quas vero tenetur laboriosam esse.
-        </div>}
+       {recipeShown && <ClaudeRecipe recipe={recipe}/>}
 
         </main>
     )
